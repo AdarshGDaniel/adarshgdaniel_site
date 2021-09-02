87e8c6d5741a821fb943eb7c0074311e3198bb1d
AdarshGDaniel <adarshgeoffrey@karunya.edu.in>
2021-09-02 21:55:32 +0530

    Updates

--- static/js/main.6472da07.chunk.js
+++
@@ -1,2 +0,0 @@
-(this.webpackJsonpadarshgdaniel_site=this.webpackJsonpadarshgdaniel_site||[]).push([[0],{11:function(e,n,t){"use strict";t.r(n);var c=t(1),s=t.n(c),i=t(3),r=t.n(i),a=(t(8),t(9),t(0));var d=function(){return Object(a.jsx)("div",{className:"App",children:Object(a.jsx)("h1",{children:"Under Construction"})})};r.a.render(Object(a.jsx)(s.a.StrictMode,{children:Object(a.jsx)(d,{})}),document.getElementById("root"))},8:function(e,n,t){},9:function(e,n,t){}},[[11,1,2]]]);
-//# sourceMappingURL=main.6472da07.chunk.js.map
